4205c820f6de062e32f1a8d2aca29e2f1510c627
DAMIAN MITRIA <damian.mitria@gmail.com>
2023-12-12 02:42:34 -0300

    elimino capeta target del repo

--- Backend/CryptoCoin/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-USE railway;
-
-INSERT INTO usuario VALUES (11, 'Damian', 'damian@gmail.com', 1234);
-INSERT INTO usuario VALUES (12, 'Pedro', 'pedro@gmail.com');
-INSERT INTO usuario VALUES (13, 'Martin', 'martin@gmail.com');
